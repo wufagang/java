798bc0249598831868f2d13f8a29a0a7365a731d
924160580@qq.com <924160580@qq.com>
2020-08-29 16:56:33 +0800

    大厂面试 之  集合类不安全 代码提交

--- interview/src/main/java/com/wfg/collection/Demo1.java
+++ interview/src/main/java/com/wfg/collection/Demo1.java
@@ -1,8 +1,10 @@
 package com.wfg.collection;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 /**
  * java
@@ -15,11 +17,61 @@ import java.util.Map;
  */
 public class Demo1 {
 
+    @Test
+    public void test1(){
+        //单线程是没问题问题的 我们非常熟悉
+        List<String> list = new ArrayList<>();
+        list.add("dd");
+        list.add("aa");
+        list.add("bb");
+        list.forEach((item)->{
+            System.out.println(item);
+        });
+    }
+
+
+
+//    public static void main(String[] args) {
+//        List<String> list = new ArrayList<>();
+//        for (int i = 1; i <=30 ; i++) {
+//            new Thread(()->{
+//                list.add(UUID.randomUUID().toString().substring(0, 8));
+//                System.out.println(Thread.currentThread().getName()+"====" +list);
+//            },String.valueOf(i)).start();
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        List<String> list=Collections.synchronizedList(new ArrayList<>());
+//        /**
+//         * 错误的写法
+//         * List<String> list = new ArrayList<>();
+//         * Collections.synchronizedList(list);
+//         */
+//
+//        for (int i = 1; i <=30 ; i++) {
+//            new Thread(()->{
+//                list.add(UUID.randomUUID().toString().substring(0, 8));
+//                System.out.println(Thread.currentThread().getName()+"====" +list);
+//            },String.valueOf(i)).start();
+//        }
+//    }
+
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        System.out.println(list.size());
-        list.add(2);
-        System.out.println(list.size());
+
+        List<String> list=new CopyOnWriteArrayList();
+        /**
+         * 错误的写法
+         * List<String> list = new ArrayList<>();
+         * Collections.synchronizedList(list);
+         */
+
+        for (int i = 1; i <=30 ; i++) {
+            new Thread(()->{
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(Thread.currentThread().getName()+"====" +list);
+            },String.valueOf(i)).start();
+        }
     }
 }
