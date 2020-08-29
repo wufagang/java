7887afdbe62aafe887953b51bec2f87971ff0c3f
924160580@qq.com <924160580@qq.com>
2020-08-29 15:19:34 +0800

    大厂面试 之  cas 代码提交

---
+++ interview/src/main/java/com/wfg/collection/Demo1.java
@@ -0,0 +1,25 @@
+package com.wfg.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * java
+ *
+ * @Title: com.wfg.collection
+ * @Date: 2020/8/27 11:09
+ * @Author: wfg
+ * @Description:
+ * @Version:
+ */
+public class Demo1 {
+
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+        list.add(2);
+        System.out.println(list.size());
+    }
+}
